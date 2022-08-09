872d89fb697a13de73d83e58ba0a69d9cb57ddf6
bugrecov <a.bugrecov@yandex.ru>
2022-08-09 20:37:02 +0300

    refactor: добавить js

---
+++ blocks/scripts/index.js
@@ -0,0 +1,7 @@
+const popup = document.querySelector('.popup');
+const openEditButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close.css');
+
+console.log('popup', popup);
+console.log('openEditButton', openEditButton);
+console.log('openEditButton', closeButton);
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let popup = document.querySelector(selectors:'.popup');
+const openEditButton = document.querySelector(selectors:'.profile__edit-button');
+const closeButton = popup.querySelector(selectors:'.popup__close.css');
+
+console.log('popup', popup);
+console.log('openEditButton', openEditButton);
+console.log('openEditButton', closeButton);
