cd301a2a6539a6b1b3eeaafc55660180d2c8526b
bugrecov <a.bugrecov@yandex.ru>
2022-08-11 20:41:21 +0300

    feat: попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,14 @@
-let popup = document.querySelector(selectors:'.popup');
-const openEditButton = document.querySelector(selectors:'.profile__edit-button');
-const closeButton = popup.querySelector(selectors:'.popup__close.css');
+const popup = document.querySelector('.popup_type_edit-profile');
+const openEditButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close');
 
 console.log('popup', popup);
 console.log('openEditButton', openEditButton);
 console.log('openEditButton', closeButton);
+
+const togglePopup = function () {
+  popup.classList.toggle('popup_opened');
+}
+
+openEditButton.addEventListener('click', togglePopup)
+closeButton.addEventListener('click', togglePopup)
