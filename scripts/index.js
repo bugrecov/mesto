1c4c3628198226193de891ebdcec9c51db933b1d
bugrecov <a.bugrecov@yandex.ru>
2022-08-26 23:24:05 +0300

    add cards js

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,45 @@ const closeButton = popup.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
-let profileName = document.querySelector('.profile__title')
-let profileJob = document.querySelector('.profile__description')
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__description');
 
-console.log('formElement', formElement)
-console.log('nameInput', nameInput)
-console.log('jobInput', jobInput)
-console.log('profileName', profileName)
-console.log('profileJob', profileJob)
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-console.log('popup', popup);
-console.log('openEditButton', openEditButton);
-console.log('openEditButton', closeButton);
+// console.log('formElement', formElement)
+// console.log('nameInput', nameInput)
+// console.log('jobInput', jobInput)
+// console.log('profileName', profileName)
+// console.log('profileJob', profileJob)
+
+// console.log('popup', popup);
+// console.log('openEditButton', openEditButton);
+// console.log('openEditButton', closeButton);
 
 const togglePopup = function () {
   popup.classList.toggle('popup_opened');
@@ -25,6 +52,8 @@ const togglePopup = function () {
 
 openEditButton.addEventListener('click', togglePopup)
 closeButton.addEventListener('click', togglePopup)
+
+// openAddCardButton.addEventListener('click', togglePopup)
 
 // Форма
 
@@ -43,14 +72,37 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 
 
-// Кнопка лайка
+// 1 Клонирование карточки
+// 2 Клонирование всех нужных элементов
+// 3 Изменение ссылки и текста
+// 4 Добавить карточку в ul
+// 5 Написать функцию для добавления карточки в ul
+// 6 Пройтись по массиву и вызвать функцию (к каждому элементу)
 
-// const like = document.querySelector('.item__like');
+function printCard(text, link) {
+  const placeCard = document.querySelector('#place-card');
+  const cardHTML = placeCard.cloneNode(true);
+  const img = cardHTML.querySelector('.item__image');
+  const cloneText = cardHTML.querySelector('.item__title');
+  
+  img.src = link;
+  cloneText.textContent = text;
+  document.querySelector('.grid-elements').append(cardHTML);
+}
 
-// console.log('like', like);
+function loopCards(){
+  initialCards.forEach((el) => {
+    printCard(el.name, el.link)
+  })
+}
 
-// const toggleLike = function () {
-//   like.classList.toggle('.item__like_active');
-// }
+loopCards();
+initialCards.push({name: 'xxx', link :'xxx'});
 
-// like.addEventListener('click', toggleLike)
+printCard('XXX','YYY');
+
+function addCard() {
+
+}
+
+// При добавлении карточки (после сделанного попапа) скопировать значение из полей и добавить их в массив (через пуш) initialCards.push({name: 'xxx', link :'xxx'})
