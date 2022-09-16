1db89b9cb50795978389d99ee2fc09e1a5173f5f
bugrecov <a.bugrecov@yandex.ru>
2022-09-16 22:56:22 +0300

    fix button disabled + feat js validate file

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,9 @@
-function showInputError = (formElement)
+const validationConfig = {
+  formSelector: ".popup__form",
+  fieldSetSelector: ".popup__fieldset",
+  inputSelector: ".popup__input",
+  inputErrorSelector: ".popup__input-error",
+  submitButtonSelector: ".popup__save-button",
+  submitButtonDisabledClass: "popup__save-button_disabled",
+  inputErrorActiveClass: "popup__input-error_active",
+}
